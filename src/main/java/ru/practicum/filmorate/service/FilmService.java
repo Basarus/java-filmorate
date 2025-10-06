ce8f76d3acfb13b33fbda5e5cd5f72d873bf5ac3
basarus51 <basarus51@gmail.com>
2025-10-06 16:13:56 +0300

    sprint 12

--- src/main/java/ru/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/practicum/filmorate/service/FilmService.java
@@ -1,7 +1,6 @@
 package ru.practicum.filmorate.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
--- src/main/java/ru/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/practicum/filmorate/service/UserService.java
@@ -1,8 +1,6 @@
 package ru.practicum.filmorate.service;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
