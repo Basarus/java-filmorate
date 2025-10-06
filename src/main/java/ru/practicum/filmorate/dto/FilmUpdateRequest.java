4655bc85c7791a55a179b76e6967fe6fa0e1b3ea
basarus51 <basarus51@gmail.com>
2025-10-06 16:40:02 +0300

    sprint 12

--- src/main/java/ru/practicum/filmorate/dto/FilmUpdateRequest.java
+++ src/main/java/ru/practicum/filmorate/dto/FilmUpdateRequest.java
@@ -1,11 +1,12 @@
 package ru.practicum.filmorate.dto;
 
 import jakarta.validation.constraints.*;
+import ru.practicum.filmorate.model.Mpa;
 
 import java.time.LocalDate;
 import java.util.Set;
 
 public record FilmUpdateRequest(@NotNull Integer id, @NotBlank String name, @Size(max = 200) String description,
                                 @NotNull LocalDate releaseDate, @Positive int duration, @NotNull Integer mpaId,
-                                Set<Integer> genreIds) {
+                                Set<Integer> genreIds, @NotNull Mpa mpa) {
 }
