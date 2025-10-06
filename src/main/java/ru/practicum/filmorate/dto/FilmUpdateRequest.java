404f52d357494e5d9801922cc522569656877618
basarus51 <basarus51@gmail.com>
2025-10-06 17:05:33 +0300

    sprint 12

--- src/main/java/ru/practicum/filmorate/dto/FilmUpdateRequest.java
+++ src/main/java/ru/practicum/filmorate/dto/FilmUpdateRequest.java
@@ -7,6 +7,6 @@ import java.time.LocalDate;
 import java.util.Set;
 
 public record FilmUpdateRequest(@NotNull Integer id, @NotBlank String name, @Size(max = 200) String description,
-                                @NotNull LocalDate releaseDate, @Positive int duration, @NotNull Integer mpaId,
-                                Set<Integer> genreIds, @NotNull Mpa mpa) {
+                                @NotNull LocalDate releaseDate, @Positive int duration, @NotNull Mpa mpa,
+                                Set<Integer> genreIds) {
 }
