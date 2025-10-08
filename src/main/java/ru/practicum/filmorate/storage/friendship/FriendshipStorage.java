8d386f6278a4d26bdf23b6a5bccaa67696565cf2
basarus51 <basarus51@gmail.com>
2025-10-08 14:39:23 +0300

    sprint 12

--- src/main/java/ru/practicum/filmorate/storage/friendship/FriendshipStorage.java
+++ src/main/java/ru/practicum/filmorate/storage/friendship/FriendshipStorage.java
@@ -6,7 +6,10 @@ import java.util.List;
 
 public interface FriendshipStorage {
     void add(int userId, int friendId);
+
     void remove(int userId, int friendId);
+
     List<User> friendsOf(int userId);
+
     List<User> commonFriends(int userA, int userB);
 }
